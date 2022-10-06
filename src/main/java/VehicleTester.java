a6876b1e9d272438658f82fcdb538fa1cde5f865
Tony Ehlert <aehlert@dmacc.edu>
2022-10-06 16:32:15 -0500

    Revert "Delete VehicleTester.java"
    This reverts commit aec4c97b659f03543922de8d58baf63905400b28.

---
+++ src/main/java/VehicleTester.java
@@ -0,0 +1,40 @@
+import java.time.LocalDate;
+import java.util.List;
+
+import controller.OwnerHelper;
+import controller.VehicleHelper;
+import model.Owner;
+import model.Vehicle;
+
+/**
+ * @author Tony Ehlert - aehlert
+ * CIS175 - Fall 2022
+ * Oct 6, 2022
+ */
+public class VehicleTester {
+
+	public static void main(String[] args) {
+		
+		LocalDate today = LocalDate.now();
+		LocalDate yesterday = LocalDate.of(2022, 10, 6);
+		
+		Owner janeSmith = new Owner("Jane", "Smith");
+		Owner johnDoe = new Owner("John", "Doe");
+		
+		Vehicle redTruck = new Vehicle(janeSmith, today, "Truck", "Red");
+		Vehicle blackCar = new Vehicle(johnDoe, yesterday, "Car", "Black");
+		
+		VehicleHelper vh = new VehicleHelper();
+		
+		vh.insertNewVehicle(redTruck);
+		vh.insertNewVehicle(blackCar);
+		
+		List<Vehicle> allVehicles = vh.showAllVehicles();
+		
+		for (Vehicle v: allVehicles) {
+			System.out.println(v.toString());
+		}
+		
+	}
+
+}
